49ee7efc759f315678224e8493872c068c19a998
meekbronsen <mikolskeezy@gmail.com>
2024-02-27 22:54:38 +0300

    Add loading Skeltons on IssuePage.

---
+++ app/issues/_components/IssueActions.tsx
@@ -0,0 +1,12 @@
+import Button from "@/app/components/Button";
+import React from "react";
+
+const IssueActions = () => {
+  return (
+    <>
+      <Button href="/issues/new">New Issue</Button>
+    </>
+  );
+};
+
+export default IssueActions;
