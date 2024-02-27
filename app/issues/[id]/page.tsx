428106d283fe2d61185c85ce0636959cc8f2cc83
meekbronsen <mikolskeezy@gmail.com>
2024-02-27 13:08:01 +0300

    Style Issue Link in the IssueTable.

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Text, Flex, Heading, Card } from "@radix-ui/themes";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex className="my-3 space-x-3">
+        <IssueStatusBadge status={issue.status}/>
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
