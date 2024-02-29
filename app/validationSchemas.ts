f4ff8ca42538694234342041a899f33c92ea1ac7
meekbronsen <mikolskeezy@gmail.com>
2024-03-01 00:38:13 +0300

    Modify PATCH issue api to assign Issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,21 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const issueValidationSchema = z.object({
-    title: z.string().min(2, 'Title is required').max(255),
-    description: z.string().min(2, 'Description is required')
+  title: z.string().min(2, "Title is required").max(255),
+  description: z.string().min(2, "Description is required").max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(2, "Title is too short").max(255).optional(),
+  description: z
+    .string()
+    .min(2, "Description is too short")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedtoUserId is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
