6c0a6c04fbefd113fb6d093184c312f7fcf75695
meekbronsen <mikolskeezy@gmail.com>
2024-03-01 16:42:30 +0300

    Stop sort query from disappering when switching a filter.

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,31 +1,41 @@
-'use client'
+"use client";
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value: Status | null }[] = [
-  { label: "All", value : null },
+  { label: "All", value: null },
   { label: "Open", value: "OPEN" },
   { label: "In Progress", value: "IN_PROGRESS" },
   { label: "Closed", value: "CLOSED" },
 ];
 
-
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const handleOnChange = (status: string | null) => {
-      const query = status ? `?status=${status}`: ''
-      router.push(`/issues/list` + query);
-  }
-  
+    const params = new URLSearchParams();
+
+    if (status) params.append("status", status);
+
+    if (searchParams.get('sortBy'))
+      params.append('sortBy', searchParams.get('sortBy')!)
+
+    const query =  params.size ? '?' + params.toString() : '';
+    router.push(`/issues/list` + query);
+  };
+
   return (
     <Select.Root onValueChange={handleOnChange}>
-      <Select.Trigger style={{cursor: "pointer"}}  placeholder="Filter by status..."></Select.Trigger>
+      <Select.Trigger
+        style={{ cursor: "pointer" }}
+        placeholder="Filter by status..."
+      ></Select.Trigger>
       <Select.Content>
-        { statuses.map((status) => (
+        {statuses.map((status) => (
           <Select.Item key={status.value} value={status.value!}>
             {status.label}
           </Select.Item>
