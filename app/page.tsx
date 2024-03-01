4c06e211b5c3d7ab9674c333c0b7f532d07763b9
meekbronsen <mikolskeezy@gmail.com>
2024-03-01 22:33:46 +0300

    Build pagination component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import NavBar from "./NavBar";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
     <>
-    <div>Hello world</div>
+    <Pagination itemsCount={100} pageSize={10} currentPage={2} />
     </>
   );
 }
