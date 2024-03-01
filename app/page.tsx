f99087874120303db8cef9652c510b17a437fb1c
meekbronsen <mikolskeezy@gmail.com>
2024-03-01 23:13:34 +0300

    Create page QueryString parameter from pagination component.

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,18 @@ import Image from "next/image";
 import NavBar from "./NavBar";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+interface Props {
+  searchParams: { page: string };
+}
+
+export default function Home({ searchParams }: Props) {
   return (
     <>
-    <Pagination itemsCount={100} pageSize={10} currentPage={2} />
+      <Pagination
+        itemsCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </>
   );
 }
