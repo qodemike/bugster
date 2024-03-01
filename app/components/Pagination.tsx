aa355779e79ff0b289403c88eafc1f183cb4c873
meekbronsen <mikolskeezy@gmail.com>
2024-03-01 23:13:34 +0300

    Create page QueryString parameter from pagination component.

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,5 +1,13 @@
-import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowDownIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from "@radix-ui/react-icons";
+"use client";
+
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
 import { Button, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
   itemsCount: number;
@@ -8,18 +16,59 @@ interface Props {
 }
 
 const Pagination = ({ itemsCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const pageCount = Math.ceil(itemsCount / pageSize);
 
   if (pageCount <= 1) return null;
 
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   return (
     <>
       <div className="flex gap-2 items-center">
-        <Text size={'2'}>Page {currentPage} of {pageCount}</Text>
-        <Button color="gray" variant="soft" disabled={currentPage === 1}> <DoubleArrowLeftIcon/></Button>
-        <Button color="gray" variant="soft" disabled={currentPage === 1}> <ChevronLeftIcon/></Button>
-        <Button color="gray" variant="soft" disabled={currentPage === pageCount}> <ChevronRightIcon/></Button>
-        <Button color="gray" variant="soft" disabled={currentPage === pageCount}> <DoubleArrowRightIcon/></Button>
+        <div className="w-24">
+        <Text size={"2"} >
+          Page {currentPage} of {pageCount}
+        </Text>
+        </div>
+        <Button
+          color={currentPage == 1 ? 'gray': "violet"}
+          variant="classic"
+          disabled={currentPage === 1}
+          onClick={() => changePage(1)}
+        >
+          <DoubleArrowLeftIcon />
+        </Button>
+        <Button
+          color="violet"
+          variant="classic"
+          disabled={currentPage === 1}
+          onClick={() => changePage(currentPage - 1)}
+        >
+          <ChevronLeftIcon />
+        </Button>
+        <Button
+          color="violet"
+          variant="classic"
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(currentPage + 1)}
+        >
+          <ChevronRightIcon />
+        </Button>
+        <Button
+          color={currentPage == pageCount ? 'gray': "violet"}
+          variant="classic"
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(pageCount)}
+        >
+          <DoubleArrowRightIcon />
+        </Button>
       </div>
     </>
   );
