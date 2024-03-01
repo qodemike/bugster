9994ef9b5c853eb8746f7b7c1e49822b8f671029
meekbronsen <mikolskeezy@gmail.com>
2024-03-01 16:11:03 +0300

    Make sort Issues functional.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import prisma from "@/prisma/client";
-import IssuesTable from "../_components/IssuesTable";
+import IssuesTable, { sortByType } from "../_components/IssuesTable";
 import IssueActions from "../_components/IssueActions";
 import { Status } from "@prisma/client";
 
-interface Props{
-  searchParams: { status: Status }
+interface Props {
+  searchParams: { status: Status; sortBy?: sortByType };
 }
+const sortOptions = ["title", "status", "createdAt"]
 
 const IssuesPage = async ({ searchParams }: Props) => {
 
+  // Make sure the status is valid before search
   const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
-  const issues = await prisma.issue.findMany({where: { status: status }});
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  // Validating the sort option
+  const sortBy = sortOptions.includes(searchParams.sortBy || '') ? { [ searchParams.sortBy! ]: 'asc'} : undefined
   
+  const issues = await prisma.issue.findMany({
+    where: { status: status },
+    orderBy: sortBy ,
+  });
+
   return (
     <div>
       <IssueActions />
