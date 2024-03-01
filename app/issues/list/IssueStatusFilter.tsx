07b1e987662b9a1bf8fb2981aec2047c33da9859
meekbronsen <mikolskeezy@gmail.com>
2024-03-01 14:18:16 +0300

    Make IssueStatusFilter Functional.

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,19 +2,28 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value: Status | null }[] = [
-  { label: "All", value : null},
+  { label: "All", value : null },
   { label: "Open", value: "OPEN" },
   { label: "In Progress", value: "IN_PROGRESS" },
   { label: "Closed", value: "CLOSED" },
 ];
 
+
 const IssueStatusFilter = () => {
+  const router = useRouter();
+
+  const handleOnChange = (status: string | null) => {
+      const query = status ? `?status=${status}`: ''
+      router.push(`/issues/list` + query);
+  }
+  
   return (
-    <Select.Root>
-      <Select.Trigger  placeholder="Filter by status..."></Select.Trigger>
+    <Select.Root onValueChange={handleOnChange}>
+      <Select.Trigger style={{cursor: "pointer"}}  placeholder="Filter by status..."></Select.Trigger>
       <Select.Content>
         { statuses.map((status) => (
           <Select.Item key={status.value} value={status.value!}>
