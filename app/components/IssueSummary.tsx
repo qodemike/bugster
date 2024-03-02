605773e3e36919e3f62d1ed8117c27b3ac38890d
meekbronsen <mikolskeezy@gmail.com>
2024-03-02 22:29:36 +0300

    Stop double Querying the database in IssueDetailsPage.

--- app/components/IssueSummary.tsx
+++ app/components/IssueSummary.tsx
@@ -23,12 +23,19 @@ const IssueSummary = ({ open, inProgress, closed }: Props) => {
   return (
     <div className="flex gap-3">
       {containers.map((container) => (
-        <Card key={container.label}>
-          <div className="flex flex-col gap-2">
-            <Link className="text-sm font-medium" href={`/issues/list?status=${container.status}`}>{container.label}</Link>
-            <Text size={"5"} className="font-bold ">{container.value}</Text>
-          </div>
-        </Card>
+        <Link
+          key={container.label}
+          href={`/issues/list?status=${container.status}`}
+        >
+          <Card >
+            <div className="flex flex-col gap-2">
+              <Text className="text-sm font-medium">{container.label}</Text> 
+              <Text size={"5"} className="font-bold ">
+                {container.value}
+              </Text>
+            </div>
+          </Card>
+        </Link>
       ))}
     </div>
   );
